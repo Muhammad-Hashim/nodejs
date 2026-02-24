f6a21b583ae429d94fcccb8e47d2e7281d501172
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2026-02-24 09:55:29 +0500

    docs: update README

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: ["main"],
+  
+  tagFormat: "${version}",
+  
+  plugins: [
+    ["@semantic-release/commit-analyzer", { preset: "angular" }],
+    ["@semantic-release/release-notes-generator", { preset: "angular" }],
+    ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
