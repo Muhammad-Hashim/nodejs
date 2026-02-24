b63f80aebb820f4a744b6b00a39d0941b1d48d81
Muhammad Hashim <muhammadhashimsardar@gmail.com>
2026-02-24 10:14:21 +0500

    revert: Remove custom template, use default semantic-release format
    - Remove complex custom template
    - Use standard angular preset
    - Keep simple, working release notes
    - Remove contributors and dependencies sections

--- release.config.cjs
+++ release.config.cjs
@@ -5,58 +5,7 @@ module.exports = {
   
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "angular" }],
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        preset: "angular",
-        writerOpts: {
-          mainTemplate: `
-## 🎉 What's New in v{{nextRelease.version}}
-
-{{#each commits}}
-{{#if (eq this.type "feat")}}
-### ✨ New Features
-- {{this.subject}}
-{{/if}}
-{{#if (eq this.type "fix")}}
-### 🐛 Bug Fixes  
-- {{this.subject}}
-{{/if}}
-{{#if (eq this.type "docs")}}
-### 📚 Documentation Updates
-- {{this.subject}}
-{{/if}}
-{{#if (eq this.type "refactor")}}
-### ♻️ Code Improvements
-- {{this.subject}}
-{{/if}}
-{{#if (eq this.type "breaking")}}
-### 💥 Breaking Changes
-- {{this.subject}}
-{{/if}}
-{{/each}}
-
----
-
-### 📦 Dependencies
-{{#if nextRelease.dependencies}}
-Updated dependencies in this release.
-{{else}}
-No dependency changes.
-{{/if}}
-
----
-
-### 👥 Contributors
-{{#each nextRelease.contributors}}
-- {{this.name}} (@{{this.username}})
-{{/each}}
-
----
-          `
-        }
-      }
-    ],
+    ["@semantic-release/release-notes-generator", { preset: "angular" }],
     ["@semantic-release/changelog", { changelogFile: "CHANGELOG.md" }],
     [
       "@semantic-release/git",
